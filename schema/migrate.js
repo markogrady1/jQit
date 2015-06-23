dea75f761eb34f1e629e2501333272e970b1ee2c
markogrady1 <markogrady.code@gmail.com>
2015-06-23 23:49:20 +0100

    remove code that abused DRY principle

--- schema/migrate.js
+++ schema/migrate.js
@@ -15,73 +15,43 @@ var connect = function(target, fn) {
 exports.repositoryMigrate = function(){
 	tmpLog.update('DATA MIGRATION', 'repositories loading...', false);
 	connect('repositories', function(db){
-
-
-	// // MongoClient.connect("mongodb://127.0.0.1:27017/repositories", function(err, db){
-	// if(err) throw err;	
-	db.collection('repos').remove({});
-	var allRepos = require('../repoData/rep');	    	
-    db.collection('repos').insert(allRepos, function(err, data){
-    
-    	if(err) throw err;
-    	
-    	tmpLog.update('DATA MIGRATION', 'jquery repositories loaded', true);
-		db.close();
-    	});
+		db.collection('repos').remove({});
+		var allRepos = require('../repoData/rep');	    	
+	    db.collection('repos').insert(allRepos, function(err, data){
+	    	if(err) throw err;
+	    	tmpLog.update('DATA MIGRATION', 'jquery repositories loaded', true);
+			db.close();
+	    });
 	});
-	// });
 }
 
-// MongoClient.connect("mongodb://127.0.0.1:27017/issues", function(err, db){
-
-	// if(err) throw err;
 connect('issues', function(db){
-
-
 	for(var i = 1; i < 49; i++){
 		var obj = require('../repoData/issues/open/' + i + "_issues");
 		if(obj == ""){
 			continue
 		}
         var url = mapping(obj, function(data) {return data.url;})
-        // var url = obj.map(function(data) {return data.url; });
-	    // var urlValue = url.toString();
 	    var dataName = splitValue(url, 5, '/');
-	    // var nameArr = urlValue.split('/');
-	    // var dataName = nameArr[5];
 	    db.collection(dataName).remove({});
         db.collection(dataName).insert(obj, function(err, data){
-
             if(err) throw err;
-
         });
 	}
 	tmpLog.update('DATA MIGRATION', 'jquery open issues loaded', true);
-// });
 });
 
 exports.pullsMigrate = function() {
-
-	// MongoClient.connect("mongodb://127.0.0.1:27017/pulls", function(err, db){
-
-		// if(err) throw err;
 	connect('pulls', function(db){
-
-
 		for(var i = 1; i < 49; i++){
 			var obj = require('../repoData/pulls/open/' + i + "_pulls");
 			if(obj == ""){
 				continue
 			}
         	var url = mapping(obj, function(data) {return data.url;})
-	        // var url = obj.map(function(data) {return data.url; });
-		    // var urlValue = url.toString();
-		    // var nameArr = urlValue.split('/');
-		    // var dataName = nameArr[5];
-		     var dataName = splitValue(url, 5, '/');
+		    var dataName = splitValue(url, 5, '/');
 		    db.collection(dataName).remove({});
 	        db.collection(dataName).insert(obj, function(err, data){
-
             if(err) throw err;
 
         	});
@@ -94,61 +64,37 @@ exports.pullsMigrate = function() {
 
 
 exports.eventsMigrate = function() {
-
-	// MongoClient.connect("mongodb://127.0.0.1:27017/events", function(err, db){
-
-		// if(err) throw err;
     connect('events', function(db){
-
-
 		for(var i = 1; i < 49; i++){
 			var obj = require('../repoData/events/' + i + "events");
 			if(obj == ""){
 				continue
 			}
         	var url = mapping(obj, function(data) {return data.url;})
-	        // var url = obj.map(function(data) {return data.url; });
-		    // var urlValue = url.toString();
-		    // var nameArr = urlValue.split('/');
-		    // var dataName = nameArr[5];
-		     var dataName = splitValue(url, 5, '/');
-		    // var formatName = dataName.split(',')
-		    // dataName = formatName[0];
-		     var dataName = splitValue(dataName, 0, ',');
+		    var dataName = splitValue(url, 5, '/');
+		    dataName = splitValue(dataName, 0, ',');
 		    db.collection(dataName).remove({});
 	        db.collection(dataName).insert(obj, function(err, data){
-
             if(err) throw err;
 
         	});
 		}
 		// db.close();
 		tmpLog.update('DATA MIGRATION', 'jquery events loaded', true);
-	// });
-});
+	});
 }
 
 exports.closedDataMigration = function(targetData){
-// MongoClient.connect("mongodb://127.0.0.1:27017/" + targetData + "Closed", function(err, db){
-
-
-// if(err) throw err;
 	connect(targetData + 'Closed', function(db){
-
-
 		for(var i = 1; i < 49; i++){
 			obj = require('../repoData/' + targetData + '/closed/' + i + "_closed_" + targetData + "");
 			if(obj == ""){
 				continue
 			}
         	var url = mapping(obj, function(data) {return data.url;})
-	        // var url = obj.map(function(data) {return data.url; });
-		    // var urlValue = url.toString();
-		    // var nameArr = urlValue.split('/');
-		    // var dataName = nameArr[5];
 		     var dataName = splitValue(url, 5, '/');
 		    db.collection(dataName).remove({});
-		    var batch = db.collection(dataName).initializeUnorderedBulkOp({useLegacyOps: false}); //enable bulk inserting of data because of high numbers
+		    var batch = db.collection(dataName).initializeUnorderedBulkOp({useLegacyOps: false}); //enable bulk inserting of data 
 		    for(var j = 0; j < obj.length; j++){	
 		    	batch.insert({  
 		    		url: obj[j].url,
@@ -163,11 +109,9 @@ exports.closedDataMigration = function(targetData){
 		    }
 		    batch.execute(function(err, result){});
 		}
-
 		db.close();
 		tmpLog.update('DATA MIGRATION', 'jquery closed ' + targetData + ' loaded', true);
-// });
-});
+	});
 }
 
 var splitValue = function(origVal, index, separator) {
--- schema/schema.js
+++ schema/schema.js
@@ -80,66 +80,80 @@ exports.getHistory = function(param, callback) {
 	});
 }
 
-exports.getOpenPullRequestData = function(param, callback) {
-	var query = {"repo": param};
-	collection = query.repo;
-	var projection = { "_id": 0 };
-	connection("pulls", function(db){
-		db.collection(collection).find({}, projection).toArray(function(err, doc){
-			if(err) throw err;
+// exports.getOpenPullRequestData = function(param, callback) {
+// 	var query = {"repo": param};
+// 	collection = query.repo;
+// 	var projection = { "_id": 0 };
+// 	connection("pulls", function(db){
+// 		db.collection(collection).find({}, projection).toArray(function(err, doc){
+// 			if(err) throw err;
 
-			db.close();
-			callback(doc);
-		});
-	});
-}
+// 			db.close();
+// 			callback(doc);
+// 		});
+// 	});
+// }
  
-exports.getClosedPullRequestData = function(param, callback) {
-	var query = {"repo": param};
-	collection = query.repo;
-	var projection = { "_id": 0 };
-	connection("pullsClosed", function(db){
-		db.collection(collection).find({}, projection).toArray(function(err, doc){
-			if(err) throw err;
+// exports.getClosedPullRequestData = function(param, callback) {
+// 	var query = {"repo": param};
+// 	collection = query.repo;
+// 	var projection = { "_id": 0 };
+// 	connection("pullsClosed", function(db){
+// 		db.collection(collection).find({}, projection).toArray(function(err, doc){
+// 			if(err) throw err;
 
-			db.close();
-			callback(doc);
-		});
-	});
-}
+// 			db.close();
+// 			callback(doc);
+// 		});
+// 	});
+// }
  
-exports.getClosedIssuesData = function(param, callback) {
-	var query = {"repo": param};
-	collection = query.repo;
-	var projection = { "_id": 0 };
-	connection("issuesClosed", function(db){
-		db.collection(collection).find({}, projection).toArray(function(err, doc){
-			if(err) throw err;
+// exports.getClosedIssuesData = function(param, callback) {
+// 	var query = {"repo": param};
+// 	collection = query.repo;
+// 	var projection = { "_id": 0 };
+// 	connection("issuesClosed", function(db){
+// 		db.collection(collection).find({}, projection).toArray(function(err, doc){
+// 			if(err) throw err;
 
-			db.close();
-			callback(doc);
-		});
-	});
-}
+// 			db.close();
+// 			callback(doc);
+// 		});
+// 	});
+// }
 
-exports.getEvents = function(param, callback){
-var query = {"repo": param};
-	collection = query.repo;
-	var projection = { "_id": 0 };
-	connection("events", function(db){
-		db.collection(collection).find({}, projection).toArray(function(err, doc){
-			if(err) throw err;
+// exports.getEvents = function(param, callback){
+// var query = {"repo": param};
+// 	collection = query.repo;
+// 	var projection = { "_id": 0 };
+// 	connection("events", function(db){
+// 		db.collection(collection).find({}, projection).toArray(function(err, doc){
+// 			if(err) throw err;
 
-			db.close();
-			callback(doc);
-		});
-	});
-}
+// 			db.close();
+// 			callback(doc);
+// 		});
+// 	});
+// }
 
 var connection = function(dbase, callback) {
 	mongoClient.connect("mongodb://127.0.0.1:27017/" + dbase, function(err, db){
 		if(err) throw err;
 		callback(db)
+	});
+}
+
+exports.executeQuery = function(database, param, callback) {
+	var query = {"repo": param};
+	collection = query.repo;
+	var projection = { "_id": 0 };
+	connection(database, function(db){
+		db.collection(collection).find({}, projection).toArray(function(err, doc){
+			if(err) throw err;
+
+			db.close();
+			callback(doc);
+		});
 	});
 }
 
