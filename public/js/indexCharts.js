689f4c9282ff8b77057e8622f8ae99c35586b313
markogrady1 <markogrady.code@gmail.com>
2015-07-26 16:19:46 +0100

    add beginning of index pie chart

---
+++ public/js/indexCharts.js
@@ -0,0 +1,24 @@
+var indexChart = function(data) {
+	width = 500;
+	height = 500;
+radius = Math.min(width, height) / 2;
+var color = d3.scale.ordinal()
+    .range(["#98abc5", "#8a89a6", "#7b6888", "#6b486b", "#a05d56", "#d0743c", "#ff8c00"]);
+
+var arc = d3.svg.arc()
+    .outerRadius(radius - 10)
+    .innerRadius(0);
+
+var pie = d3.layout.pie()
+    .sort(null)
+    .value(function(d) { return d.population; });
+
+var svg = d3.select(".pie").append("svg")
+    .attr("width", width)
+    .attr("height", height)
+  .append("g")
+    .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
+
+
+
+}
