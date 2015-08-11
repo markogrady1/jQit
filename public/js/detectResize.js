92ed152ee59b6cb852414beb6a8cc47acc3799de
markogrady1 <markogrady.code@gmail.com>
2015-08-11 17:08:15 +0100

    add extra size queries for reponsive charts

--- public/js/detectResize.js
+++ public/js/detectResize.js
@@ -3,54 +3,114 @@ var detectWindowSize = function() {
 	console.log(document.body.clientWidth);
 
 	var clientWidth = document.body.clientWidth;
-	if(clientWidth <= 400 && v.length > 5) {
+	if(clientWidth <= 461 && v.length > 5) {
 		console.log('mobile')
+		v = temp;
 		v = v.slice(-8)
 		pr = pr.slice(-8)
 		chartWidth = 400;
 		barChartHeight = 300;
 		lineChartHeight = 300;
-		scope = "Last 8 Days";
-	} else if (clientWidth > 400 && clientWidth < 1000) {
+		standard = false;
+		scope = 'last 8 Days';
+
+	} else if (clientWidth > 461 && clientWidth <= 657) {
+		v = temp;
+		v = v.slice(-10)
+		pr = pr.slice(-10)
+		chartWidth = 500;
+		barChartHeight = 400;
+		lineChartHeight = 300;
+		standard = false;
+		scope = 'Last 10 Days';
+	} else if (clientWidth > 657 && clientWidth <= 860) {
 		console.log('tablet')
-		v = v.slice(-15)
-		pr = pr.slice(-15)
+		v = temp;
+		v = v.slice(-12)
+		pr = pr.slice(-12)
+		$text = $('text > .line-x-axis-title');
+		$text.text('last 15 days');
+		$text.css('color', 'red');
 		chartWidth = 700;
 		barChartHeight = 450;
-		lineChartHeight = 320;
-		scope = "Last 15 Days";
-	} else {
-		scope = "Last 30 Days";
+		lineChartHeight = 300;
+		standard = false;
+		scope = 'Last 12 Days';
+	} else if (clientWidth > 860 && clientWidth < 1064) {
+		console.log('tablet')
+		v = temp;
+		v = v.slice(-15)
+		pr = pr.slice(-15)
+		$text = $('text > .line-x-axis-title');
+		$text.text('last 15 days');
+		$text.css('color', 'red');
+		chartWidth = 900;
+		barChartHeight = 500;
+		lineChartHeight = 300;
+		standard = false;
+		scope = 'Last 15 Days';
+	} else if(clientWidth > 1064){
+		standard = true;
 		chartWidth = 1100;
 		barChartHeight = 550;
 		lineChartHeight = 350;
+		scope = 'Last 30 Days';
 	}
 
 }
 
 
 var repaint = function() {
-	console.log(document.body.clientWidth);
-
+	// console.log(document.body.clientWidth);
+	// var temp = v;
+	// var temp2 = pr;
+	var standard = true;
 	var clientWidth = document.body.clientWidth;
-	if(clientWidth <= 400 && v.length > 5) {
+	if(clientWidth <= 461 && v.length > 5) {
 		console.log('mobile')
+		v = temp;
 		v = v.slice(-8)
 		pr = pr.slice(-8)
 		chartWidth = 400;
 		barChartHeight = 300;
 		lineChartHeight = 300;
-	} else if (clientWidth > 400 && clientWidth < 1000) {
+	standard = false;
+
+
+	} else if (clientWidth > 461 && clientWidth <= 657) {
+		v = temp;
+		v = v.slice(-10)
+		pr = pr.slice(-10)
+		chartWidth = 500;
+		barChartHeight = 400;
+		lineChartHeight = 300;
+		standard = false;
+	} else if (clientWidth > 657 && clientWidth <= 860) {
 		console.log('tablet')
+		v = temp;
+		v = v.slice(-12)
+		pr = pr.slice(-12)
+		$text = $('text > .line-x-axis-title');
+		$text.text('last 15 days');
+		$text.css('color', 'red');
+		chartWidth = 700;
+		barChartHeight = 450;
+		lineChartHeight = 300;
+		standard = false;
+	} else if (clientWidth > 860 && clientWidth < 1064) {
+		console.log('tablet')
+		v = temp;
 		v = v.slice(-15)
 		pr = pr.slice(-15)
 		$text = $('text > .line-x-axis-title');
 		$text.text('last 15 days');
 		$text.css('color', 'red');
-		// var text = document.getElementsByClassName('x-axis-title');
-		// console.log(text[0])
-		// text[0].innerHTML = 'Last 15 Days';
-	} else if(clientWidth > 1000){
+		chartWidth = 900;
+		barChartHeight = 500;
+		lineChartHeight = 300;
+		standard = false;
+	} else if(clientWidth > 1064){
+		standard = true;
 		chartWidth = 1100;
 		barChartHeight = 550;
 		lineChartHeight = 350;
@@ -129,16 +189,29 @@ issuesBarData = {
 	dataset = [] ;
 	var n = [];
 	var m = [];
-for(var k in v) {
-   n.push({'date': v[k].date, 'issues': v[k].issues});
-    // dataset.push(v[k].issues);
-}
+	if(standard) {
+		for(var k in temp) {
+		   n.push({'date': temp[k].date, 'issues': temp[k].issues});
+		    // dataset.push(v[k].issues);
+		}
+		for(var y in temp2) {
+		  m.push({'date': temp2[y].date, 'pulls': temp2[y].pulls});
+		    //dataset.push(pr[y].pulls);
+		}
+	} else {
 
-for(var y in pr) {
-  m.push({'date': pr[y].date, 'pulls': pr[y].pulls});
-    //dataset.push(pr[y].pulls);
+
+		for(var k in v) {
+		   n.push({'date': v[k].date, 'issues': v[k].issues});
+		    // dataset.push(v[k].issues);
+		}
+
+		for(var y in pr) {
+		  m.push({'date': pr[y].date, 'pulls': pr[y].pulls});
+		    //dataset.push(pr[y].pulls);
+		}
 }
-	setBarChart(n, issuesBarData)
+setBarChart(n, issuesBarData)
 setLineChart(n, issuesLineData)
 setBarChart(m, pullsBarData)
 setLineChart(m, pullsLineData)
